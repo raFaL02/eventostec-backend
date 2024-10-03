0cefd74637e34b9cd5cb8311ef7bcbedb7d86dce
Rafael de Lima <rafalimabcl@gmail.com>
2024-10-03 12:09:30 -0300

    feat: initial commit

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.domain.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String uf;
+    private String city;
+
+    @OneToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
